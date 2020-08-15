3cece70a23ff496299156fc379203aa1cdc5b193
Gustavo Piá <piagja@gmail.com>
2020-08-15 18:03:27 -0300

    Updates

---
+++ precache-manifest.64048672dfdccfaa01b6a24693f53abf.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1b7ccd2368c2305f95e05bdbaf06d9f2",
+    "url": "/covid-tracker/index.html"
+  },
+  {
+    "revision": "b1e825dc396a34c7fa44",
+    "url": "/covid-tracker/static/css/main.82303fd3.chunk.css"
+  },
+  {
+    "revision": "7427ae441098b8d9f151",
+    "url": "/covid-tracker/static/js/2.6c4191e1.chunk.js"
+  },
+  {
+    "revision": "1ef8d540b3072f991ff256dfc61c7bcd",
+    "url": "/covid-tracker/static/js/2.6c4191e1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b1e825dc396a34c7fa44",
+    "url": "/covid-tracker/static/js/main.6b9263ea.chunk.js"
+  },
+  {
+    "revision": "a2c09be945803fa375fb",
+    "url": "/covid-tracker/static/js/runtime-main.948b6ba1.js"
+  },
+  {
+    "revision": "d3a50c83ee911d78e0fa8f6e3f96cf45",
+    "url": "/covid-tracker/static/media/covid19.d3a50c83.svg"
+  }
+]);
